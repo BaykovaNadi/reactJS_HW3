4a823f8e96ba6e53dbf6b77711c1192b43934cd1
BaykovaNadi <BaykovaNadi@yandex.ru>
2018-04-02 00:36:57 +0300

    commit 1

---
+++ src/client/app/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './components/header';
+
+class App extends Component {
+    render() {
+        return <div>Helloworld from ReactJS111111111111</div>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
